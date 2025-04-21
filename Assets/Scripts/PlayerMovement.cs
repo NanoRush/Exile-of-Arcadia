310fc7dce8ea2ac0f722555a2c8986a8a132205e
NanoRush <shayan_moid@yahoo.com>
2025-04-21 17:00:40 -0500

    movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private float horizontal;
+    private float moveSpeed = 5f;
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+    }
+
+}
