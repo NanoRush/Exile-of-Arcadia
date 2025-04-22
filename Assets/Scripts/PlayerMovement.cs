4e89e6b1ca3f2e6c9ed625471ae915fffc22f164
NanoRush <shayan_moid@yahoo.com>
2025-04-22 01:45:44 -0500

    changes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,26 @@ public class PlayerMovement : MonoBehaviour
 {
 
     private float horizontal;
-    private float moveSpeed = 5f;
+    private float moveSpeed = 8f;
+    [SerializeField] float jumpPower;
+
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    bool isGrounded;
+
+    private int jumpCount = 0;
+
+    //Wall Jump
+    public Transform wallCheck;
+    public LayerMask wallLayer;
+    bool isWallTouch;
+    bool isSliding;
+    public float wallSlidingSpeed;
+    public float wallJumpDuration = 0.1f;
+    public Vector2 wallJumpForce;
+    bool wallJumping;
+
+    bool isFacingRight = true;
 
     Rigidbody2D rb;
     // Start is called before the first frame update
@@ -19,11 +39,77 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
+        FlipSprite();
+
+        isGrounded = Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1.28f, 0.48f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+        isWallTouch = Physics2D.OverlapBox(wallCheck.position, new Vector2(0.38f, 2.46f), 0, wallLayer);
+
+        if (isGrounded)
+        {
+            jumpCount = 1;
+        }
+
+        if(isWallTouch && !isGrounded && horizontal != 0f)
+        {
+            isSliding = true;
+        }
+        else
+        {
+            isSliding = false;
+        }
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+
+            if (isSliding)
+            {
+                wallJumping = true;
+                Invoke("StopWallJump", wallJumpDuration);
+            }
+            else if (jumpCount < 2)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, 0f);
+                //rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+                rb.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
+                jumpCount++;
+            }
+        }
+
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+
+        if (isSliding)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+
+        if (wallJumping)
+        {
+            rb.velocity = new Vector2(-horizontal * wallJumpForce.x, wallJumpForce.y);
+        }
+
+
+    }
+
+    void StopWallJump()
+    {
+        wallJumping = false;
+    }
+
+    void FlipSprite()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
+
     }
 
 }
