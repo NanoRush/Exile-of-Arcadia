7d2851f5d21dd3a380e8fcdff8403d0d048793a0
NanoRush <shayan_moid@yahoo.com>
2026-02-24 20:17:59 -0600

    electro blocks

---
+++ Assets/Scripts/CameraTriggerScript.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using Cinemachine;
+
+
+public class CameraTriggerScript : MonoBehaviour
+{
+
+    public CinemachineVirtualCamera virtualCamera;
+    public float yOffsetAmount;
+    public float xOffsetAmount;
+
+    private CinemachineFramingTransposer framingTransposer;
+    private Vector3 originialOffset;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        framingTransposer = virtualCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
+        originialOffset = framingTransposer.m_TrackedObjectOffset;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+            Vector3 newOffset = originialOffset;
+            newOffset.y += yOffsetAmount;
+            newOffset.x += xOffsetAmount;
+            framingTransposer.m_TrackedObjectOffset = newOffset;
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            framingTransposer.m_TrackedObjectOffset = originialOffset;
+        }
+    }
+
+}
